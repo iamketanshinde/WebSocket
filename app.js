30090e769fbd07b24baefba6fb38fddea9f62fca
Ketan Shinde <ketan_shinde@hotmail.com>
2025-02-10 17:50:39 +0530

    server created at PORT 9000

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require("express");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
+
+
+
+server.listen(9000, ()=> console.log(`Server started at PORT 9000!`));
