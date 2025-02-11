9b146dcf6cf37140aab4e29b3bca87ca122b9e70
Ketan Shinde <ketan_shinde@hotmail.com>
2025-02-11 23:46:56 +0530

    get req added to index.html page

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
+
+
 
 const app = express();
 const server = http.createServer(app);
 
 
 
-app.use(express.static('/public'))
+app.use(express.static(path.resolve("./public")))
 
+
+app.get("/",(req,res)=>{
+    return res.sendFile("/public/index.html")
+})
 
 server.listen(9000, ()=> console.log(`Server started at PORT 9000!`));
